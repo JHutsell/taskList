396318458690040976ea0efe604338d6c2d857e1
Justin Hutsell <hutselljustin@gmail.com>
2020-02-04 16:57:09 -0500

    adds react-calendar for basic interface

--- tasklist-client/src/App.js
+++ tasklist-client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Calendar from 'react-calendar';
 
 function App() {
   return (
@@ -8,8 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Manage all of your Daily Tasks
         </p>
+        <Calendar />
         <a
           className="App-link"
           href="https://reactjs.org"
