df766d0bff1ea647b81181c215bc6f3fdc983cb0
Justin Hutsell <hutselljustin@gmail.com>
2020-02-06 16:53:22 -0500

    adds react router, basic components

--- tasklist-client/src/App.js
+++ tasklist-client/src/App.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import ToDoList from './components/ToDoList';
+import EditTodo from './components/EditToDo';
+import CreateTodo from './components/CreateToDo'
 import Calendar from 'react-calendar';
+import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Manage all of your Daily Tasks
-        </p>
-        <Calendar />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Manage all of your Daily Tasks</h2>
+          <Calendar />
+          {/* <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+      </div>
+      <Route path="/" exact component={ToDoList} />
+      <Route path="/edit/:id" component={EditTodo} />
+      <Route path="/create" component={CreateTodo} />
+    </Router>
   );
 }
 
