0570ea45214119be4529e5517d035e0cd553def5
Justin Hutsell <hutselljustin@gmail.com>
2020-02-17 15:58:18 -0500

    adds full radio button state manipulation and submit button to create todo

--- tasklist-client/src/components/CreateToDo.js
+++ tasklist-client/src/components/CreateToDo.js
@@ -17,22 +17,32 @@ export default class CreateTodo extends Component {
         })
     }
 
+    handleRadioSelection = (e) => {
+        this.setState({
+            todo_priority: e.target.value
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Welcome to Create Todo Component!!</h2>
                 <form>
                     <h4>Title</h4>
-                    <input/>
+                    <input onChange={ this.handleInputChange } value={ this.state.todo_title} type="text" name="todo_title"/>
                     <h4>Description</h4>
-                    <input />
+                    <input onChange={ this.handleInputChange } value={ this.state.todo_description } type="text" name="todo_description" />
                     <h4>Priority</h4>
-                    <input type="radio" style={{display: "inline"}}/>
-                    <label for="Low"> Low</label>
-                    <input type="radio" style={{display: "inline"}}/>
-                    <label for="Medium"> Medium</label>
-                    <input type="radio" style={{display: "inline"}}/>
-                    <label for="High"> High</label>
+                    <label for="Low"> Low 
+                    <input type="radio" onChange={ this.handleRadioSelection } value="Low" checked={ this.state.todo_priority === "Low"}/>
+                    </label>
+                    <label for="Medium"> Medium
+                    <input type="radio" onChange={ this.handleRadioSelection } value="Medium" checked={ this.state.todo_priority === "Medium"}/>
+                    </label>
+                    <label for="High"> High
+                    <input type="radio" onChange={ this.handleRadioSelection } value="High" checked={ this.state.todo_priority === "High"}/>
+                    </label><br />
+                    <Button type="submit" variant="primary">Create New Task</Button>
                 </form>
             </div>
         )
