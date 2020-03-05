9421598e779f9ff2f94cab32eb539d1b8162a298
Justin Hutsell <hutselljustin@gmail.com>
2020-03-05 17:56:33 -0500

    adds post new todo functionality

--- tasklist-client/src/components/CreateToDo.js
+++ tasklist-client/src/components/CreateToDo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 
 
@@ -23,11 +24,38 @@ export default class CreateTodo extends Component {
         })
     }
 
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log('Form Submitted');
+        console.log(`Todo Title: ${this.state.todo_title}`);
+        console.log(`Todo Description: ${this.state.todo_description}`);
+        console.log(`Todo Priority: ${this.state.todo_priority}`);
+
+        const newTodo = {
+            todo_title: this.state.todo_title,
+            todo_description: this.state.todo_description,
+            todo_priority: this.state.todo_priority,
+            todo_completed: this.state.todo_completed
+        };
+
+        axios.post('http://localhost:4000/todos/add', newTodo)
+            .then(res => console.log(res.data));
+
+        this.setState({
+            todo_title: '',
+            todo_description: '',
+            todo_priority: '',
+            todo_completed: false
+        })
+
+    }
+
     render() {
         return (
             <div>
                 <h2>Welcome to Create Todo Component!!</h2>
-                <form>
+                <form onSubmit={ this.handleSubmit }>
                     <h4>Title</h4>
                     <input onChange={ this.handleInputChange } value={ this.state.todo_title} type="text" name="todo_title"/>
                     <h4>Description</h4>
