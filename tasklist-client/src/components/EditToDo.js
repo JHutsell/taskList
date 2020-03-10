162182e02da185640aaf8484a5c0564c8d81e9ec
Justin Hutsell <hutselljustin@gmail.com>
2020-03-10 14:47:42 -0400

    adds form for edit todo

--- tasklist-client/src/components/EditToDo.js
+++ tasklist-client/src/components/EditToDo.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+
 
 export default class EditTodo extends Component {
 
@@ -29,10 +31,39 @@ export default class EditTodo extends Component {
             })
     }
 
+    handleInputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value 
+        })
+    }
+
+    handleRadioSelection = (e) => {
+        this.setState({
+            todo_priority: e.target.value
+        })
+    }
+
     render() {
         return (
             <div>
-                <p>Welcome to Edit Todo Component!!</p>
+                <h3>Edit this Todo!</h3>
+                <form>
+                    <h4>Title</h4>
+                    <input onChange={ this.handleInputChange } value={ this.state.todo_title} type="text" name="todo_title"/>
+                    <h4>Description</h4>
+                    <input onChange={ this.handleInputChange } value={ this.state.todo_description } type="text" name="todo_description" />
+                    <h4>Priority</h4>
+                    <label for="Low"> Low 
+                        <input type="radio" onChange={ this.handleRadioSelection } value="Low" checked={ this.state.todo_priority === "Low"}/>
+                    </label>
+                    <label for="Medium"> Medium
+                        <input type="radio" onChange={ this.handleRadioSelection } value="Medium" checked={ this.state.todo_priority === "Medium"}/>
+                    </label>
+                    <label for="High"> High
+                        <input type="radio" onChange={ this.handleRadioSelection } value="High" checked={ this.state.todo_priority === "High"}/>
+                    </label><br />
+                    <Button type="submit" variant="primary">Edit Task</Button>
+                </form>
             </div>
         )
     }
